ddb1df5b12faf5bbe0189ec50699c5189422915c
Reagan Charana <rgncharana2@gmail.com>
2023-05-08 08:28:28 +0300

    feat: setup fsnotify to handle file changes

---
+++ cmd/filewatch/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	paths := []string{
+		"/Users/reagan/Documents/dev/reagan/filewatch/tmp",
+	}
+
+	if len(paths) < 1 {
+		exit("must specify at least one path to watch")
+	}
+
+	// Create a new watcher.
+	w, err := fsnotify.NewWatcher()
+	if err != nil {
+		exit("creating a new watcher: %s", err)
+	}
+	defer w.Close()
+
+	// Start listening for events.
+	go watchLoop(w)
+
+	// Add all paths from the commandline.
+	for _, p := range paths {
+		err = w.Add(p)
+		if err != nil {
+			exit("%q: %s", p, err)
+		}
+	}
+
+	printTime("ready; press ^C to exit")
+	<-make(chan struct{}) // Block forever
+}
+
+func watchLoop(w *fsnotify.Watcher) {
+	i := 0
+	for {
+		select {
+		// Read from Errors.
+		case err, ok := <-w.Errors:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+				return
+			}
+			printTime("ERROR: %s", err)
+		// Read from Events.
+		case e, ok := <-w.Events:
+			if !ok { // Channel was closed (i.e. Watcher.Close() was called).
+				return
+			}
+
+			// Just print the event nicely aligned, and keep track how many
+			// events we've seen.
+			i++
+			printTime("%3d %s", i, e)
+		}
+	}
+}
+
+func printTime(s string, args ...interface{}) {
+	fmt.Printf(time.Now().Format("15:04:05.0000")+" "+s+"\n", args...)
+}
+
+func exit(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, filepath.Base(os.Args[0])+": "+format+"\n", a...)
+	fmt.Print("\n" + usage)
+	os.Exit(1)
+}
+
+var usage = `
+filewatch is a Go CLI tool that watches files and directories for changes.
+
+Commands:
+		[coming soon]
+`[1:]
