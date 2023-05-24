6629667c21464b74487d9d37fd250fee35c95764
Reagan Charana <rgncharana2@gmail.com>
2023-05-24 21:04:41 +0300

    chore: testing new cli

--- cmd/filewatch/main.go
+++ cmd/filewatch/main.go
@@ -1,20 +1,26 @@
 package main
 
-import (
-	"log"
+import "fmt"
 
-	sqlite "github.com/reagancn/filewatch/pkg/database"
-)
+// import (
+// 	"log"
+
+// 	sqlite "github.com/reagancn/filewatch/pkg/database"
+// )
 
 func main() {
-	// Initialize the database
-	db, err := sqlite.ConnectDB()
+	// // Initialize the database
+	// db, err := sqlite.ConnectDB()
 
-	if err != nil {
-		log.Println("Error connecting to database")
-		log.Fatal(err)
-	}
+	// if err != nil {
+	// 	log.Println("Error connecting to database")
+	// 	log.Fatal(err)
+	// }
 
-	// Initialize the CLI
-	RunCLI(db)
+	// // Initialize the CLI
+	// RunCLI(db)
+
+	fmt.Printf("Welcome to %s 🥳! With filewatch you can monitor multiple files and directories for changes.\n", titleStyle.Render("filewatch"))
+	fmt.Print("You can also set a custom webhook that will be triggered in the case of any event\n\n")
+	bubble()
 }
